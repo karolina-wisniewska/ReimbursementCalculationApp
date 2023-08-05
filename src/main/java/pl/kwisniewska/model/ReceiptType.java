ef6d5e6042a42aa4c5acffbc37dab1f549d4e6d8
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-08-05 21:45:06 +0200

    Improve imports

--- src/main/java/pl/kwisniewska/model/ReceiptType.java
+++ src/main/java/pl/kwisniewska/model/ReceiptType.java
@@ -1,6 +1,13 @@
 package pl.kwisniewska.model;
 
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
